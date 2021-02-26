5220fbc3d9947599514efa5da91c6ccd01b32078
ImHwaryeon <ghkfus1603@naver.com>
2021-02-26 15:37:06 +0900

    ..

--- routes/api-route.js
+++ routes/api-route.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const moment = require('moment');
+const path = require('path');
+const fs = require('fs-extra');
+const ip = require('request-ip');
+const { upload, imgExt } = require('../modules/multer');
+const { pool, sqlGen: sql } = require('../modules/mysql-pool');
+const { err, alert, extName, srcPath, realPath } = require('../modules/util');
+const pagers = require('../modules/pager');
+const { isUser, isGuest } = require('../modules/auth');
 const router = express.Router();
 
-
+router.get('/list', async (req, res, next) => {
+	try {
+		let sql, value, r, rs;
+		sql = 'SELET * FROM board ORDER BY id DESC';
+		r = await pool.query(sql);
+		res.json(r[0]);
+	}
+	catch(e) {
+		res.json(e || e.message);
+	}
+});
 
 module.exports = router;
